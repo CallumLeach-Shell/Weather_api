2316c0334ddfa97f2ffd1f824847f5ef5093c1b0
Callum Leach <Callum.Leach@shell.com>
2023-01-20 18:31:35 +0000

    use api call to download based off parameters

--- api.py
+++
@@ -1,2 +0,0 @@
-### This file will be to run the api, and create endpoints for the user etc. ###
-
--- data.py
+++ data.py
@@ -1,2 +1,20 @@
 ### This file will be to gather the data from relevant sources and perform the relevant transformations.
 
+from acccmip6.access_cm import SearchCmip6
+from acccmip6.download_dat import DownloadCmip6
+
+
+### Arguments ###
+model = "ACCESS-ESM1-5"
+variable = "sfcWind"
+experiment = "ssp126"
+rlzn = "34"
+
+dl_dir = ""
+
+# This command with search the database given specified arguments
+#SearchCmip6(model = "ACCESS-ESM1-5", variable = "sfcWind", experiment = "ssp126", rlzn = "34")
+
+# download the data to .nc files given the specified arguments
+DownloadCmip6(model = model, variable = variable, experiment = experiment, rlzn = rlzn, dl_dir=dl_dir)
+
