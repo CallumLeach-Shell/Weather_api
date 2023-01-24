06487fea51d5879aa83ed85e4f6069a7c756d9c8
Callum Leach <Callum.Leach@shell.com>
2023-01-24 11:37:33 +0000

    added yaml argument parsing

--- data.py
+++ data.py
@@ -2,19 +2,20 @@
 
 from acccmip6.access_cm import SearchCmip6
 from acccmip6.download_dat import DownloadCmip6
+import yaml
 
+# Here we load the configuration file, which passes the parameters into the downloader.
+with open('config.yaml') as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
-### Arguments ###
-model = "ACCESS-ESM1-5"
-variable = "sfcWind"
-experiment = "ssp126"
-rlzn = "34"
+# the model parameter can be passed as a list of comma seperated variables, this checks for that.
+if type(config['model']) is list:
+    model = ','.join(config['model'])
+else:
+    model = config['model']
 
-dl_dir = ""
+# download the data to .nc files given the specified arguments
+DownloadCmip6(model = model, variable = config['variable'], experiment = config['experiment'], rlzn = config['rlzn'], dl_dir = config['data_directory'])
 
 # This command with search the database given specified arguments
 #SearchCmip6(model = "ACCESS-ESM1-5", variable = "sfcWind", experiment = "ssp126", rlzn = "34")
-
-# download the data to .nc files given the specified arguments
-DownloadCmip6(model = model, variable = variable, experiment = experiment, rlzn = rlzn, dl_dir=dl_dir)
-
